91fdad95bb0d6aec57d935f3d5591a7ebdac94e9
Gabe Martinez <mgabemartinez@gmail.com>
2015-05-15 17:23:29 -0700

    Grocery List Challenge

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,8 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(one, two, three, four) {
+  return [one, two, three, four];
+};
 
-module.exports.firstAndLast = undefined;
+
+module.exports.firstAndLast = function(array) {
+  return [array[0], array[array.length - 1]];
+};
--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,14 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function (item, array) {
+  
+  if (array.indexOf(item) === -1) {
+    array.splice(array.length + 1, 0, item);
+  }
+  return array;
+};
 
-module.exports.reverseSortedList = undefined;
+
+module.exports.reverseSortedList = function (array){
+
+  return array.sort().reverse();
+
+}
